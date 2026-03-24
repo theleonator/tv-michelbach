ab131dc1d9d149f0dca5a9c0b9947ef3ede1af19
theleonator <144523430+theleonator@users.noreply.github.com>
2026-03-24 08:35:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
 export default defineConfig(({ mode }) => ({
-  base: "/",       // ← WICHTIG für Custom Domains!
+  base: "/",
   server: {
     host: "::",
     port: 8080,
