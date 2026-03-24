67a0e9ee5ec9ff7a15c0458711ff9940ca2730b2
theleonator <144523430+theleonator@users.noreply.github.com>
2026-03-24 10:23:25 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -32,7 +32,7 @@ const Index = () => {
           <h1 className="text-4xl md:text-6xl font-bold mb-4 max-w-2xl leading-tight">
             Bewegung. Gemeinschaft. Leidenschaft.
           </h1>         
-            <p className="text-lg text-muted-foreground leading-relaxed">
+            <p className="text-lg text-white leading-relaxed">
             Seit 1903. inklusiv. progressiv. verantwortungsbewusst.<br />
             In Michelbach.
             </p>
